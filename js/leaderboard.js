729425018e39a14bad8160487220a0995226ab91
berkott <berkott@gmail.com>
2018-04-07 00:49:45 -0400

    removing console log

--- js/leaderboard.js
+++ js/leaderboard.js
@@ -26,15 +26,11 @@ if (localStorage.getItem("storKey") === null) {
 
 
 function writeScore(score, nick) {
-  console.log("writeScore ",score," ",nick);
     // A post entry.
     var postData = {
         score: score,
         name: nick
     };
-    console.log("refchild");
-var t5 = myRef.child(key);
-console.log(t5);
     return myRef.child(key).set(postData);
 }
 
